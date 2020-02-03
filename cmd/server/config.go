50638b2973827a54c6770c65010ea045ee1c3604
Thomas Sauvaget <sauvaget.thomas@gmail.com>
2020-02-03 19:08:14 +0100

    Change config cachettl to time.Duration

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -1,10 +1,12 @@
 package main
 
+import "time"
+
 //Config is the main configuration struct.
 type Config struct {
-	Debug          bool   `default:"false"`
-	HTTPListenPort int    `default:"8888"`
-	CacheType      string `default:"memory"`
-	RedisURI       string `default:"localhost"`
-	CacheTTL       string `default:"300000"`
+	Debug          bool          `default:"false"`
+	HTTPListenPort int           `default:"8888"`
+	CacheType      string        `default:"memory"`
+	RedisURI       string        `default:"localhost"`
+	CacheTTL       time.Duration `default:"5m"`
 }
